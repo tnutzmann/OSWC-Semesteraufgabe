eeb242b41bca3ecd924253c84a73d622154d0544
tnutzmann <tonynutzmann@icloud.com>
2022-04-23 09:05:07 +0200

    create action added

--- src/presentation_layer/todo_list.py
+++ src/presentation_layer/todo_list.py
@@ -18,14 +18,42 @@ def all_todos_to_html():
         todos_html += todo_to_html(t)
     return todos_html
 
-# Just for testing
-def print_all():
+
+def print_todo_list():
     print ("<h1>TODO List</h1>")
     print ("<div>")
     print (all_todos_to_html())
     print ("</div>")
 
+def perform_action_create(form: cgi.FieldStorage):
+        title = form.getvalue('title')
+        content = form.getvalue('content')
+        color = form.getvalue('color')
+
+        if color != 'None':
+            todo = Todo(title, content, color=color)
+        else:
+            todo = Todo(title, content)
+        
+        db = Database('todo.db')
+        db.add_todo(todo)
+
+def perform_action(form: cgi.FieldStorage):
+    action = form.getvalue('action')
+    match action:
+        case 'create':
+            return 'create'
+        case 'delete':
+            return 'delete'
+        case 'update':
+            return 'update'
+        case 'shift':
+            return 'shift'
+
+
 def draw(form: cgi.FieldStorage):
+    perform_action(form)
+
     print('Content-Type:text/html\n')
     print('<!DOCTYPE html>')
     print('<html lang="de">')
@@ -33,8 +61,8 @@ def draw(form: cgi.FieldStorage):
     print ("<title>TODO List</title>")
     print ("</head>")
     print ("<body>")
-    
-    print_all()
+
+    print_todo_list()
 
     print ("</body>")
     print ("</html>")
