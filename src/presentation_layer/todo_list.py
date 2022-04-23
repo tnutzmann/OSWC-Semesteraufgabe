23645aadbc23ea21782776316fe4c92bc4d50e17
Sebastian Feustel <seba98feustel@gmail.com>
2022-04-23 15:11:28 +0200

    css nicht schön aber selten

--- src/presentation_layer/todo_list.py
+++ src/presentation_layer/todo_list.py
@@ -1,85 +1,103 @@
-import cgi
+'''
+Contains all the functions that are necessary to create the website.
+'''
 
+import cgi
 from data_layer.todo_database import Todo
 from data_layer.todo_database import Database
 
 def todo_to_html(todo: Todo):
+    '''
+   Create the Todo Card Div.
+    '''
     return f'''
     <div class="todo {todo.color} state_{todo.is_done}" id="todo_{todo.card_id}">
+        <h4> #{todo.card_id} </h4>
         <h2>{todo.title}</h2>
         <p>{todo.content}</p>
     </div>'''
 
 def all_todos_to_html():
-    db = Database('todo.db')
-    todos = db.get_all_todos()
+    '''
+    Create all Todo Cards
+    '''
+    database = Database('todo.db')
+
+    todos = database.get_all_todos()
     todos_html = ""
-    for t in todos:
-        todos_html += todo_to_html(t)
+
+    for todo in todos:
+        todos_html += todo_to_html(todo)
     return todos_html
 
-
 def print_todo_list():
+    '''
+    Print all Tddo Cards in Html
+    '''
     print ("<h1>TODO List</h1>")
     print ("<div>")
     print (all_todos_to_html())
     print ("</div>")
 
 def perform_action_create(form: cgi.FieldStorage):
-        db = Database('todo.db')
+    '''
+    Created todo entry in database, via url
+    '''
 
-        title = form.getvalue('title')
-        content = form.getvalue('content')
-        color = form.getvalue('color')
+    database = Database('todo.db')
 
-        if color != 'None':
-            todo = Todo(title, content, color=color)
-        else:
-            todo = Todo(title, content)
-        
-        db.add_todo(todo)
+    title = form.getvalue('title')
+    content = form.getvalue('content')
+    color = form.getvalue('color')
+
+    if color != 'None':
+        todo = Todo(title, content, color=color)
+    else:
+        todo = Todo(title, content)
+
+    database.add_todo(todo)
 
 def perform_action_delete(form: cgi.FieldStorage):
-    db = Database('todo.db')
+    '''
+    Delete todo entry in database, via url
+    '''
+    database = Database('todo.db')
     todo_id = form.getvalue('id')
-    db.remove_todo(todo_id)
+    database.remove_todo(todo_id)
 
 def perform_action_update(form: cgi.FieldStorage):
-    db = Database('todo.db')
+    '''
+    Update todo entry in database, via url
+    '''
+    database = Database('todo.db')
     todo_id = form.getvalue('id')
 
-    todo: Todo = db.get_todo(todo_id)
+    todo: Todo = database.get_todo(todo_id)
 
     todo.title = form.getvalue('title')
     todo.content = form.getvalue('content')
     todo.color = form.getvalue('color')
 
-    db.update_todo(todo)
+    database.update_todo(todo)
 
 def perform_action_shift(form: cgi.FieldStorage):
-    db = Database('todo.db')
+    '''
+    Sets the status of the todo carte to the next level
+    '''
+    database = Database('todo.db')
     todo_id = form.getvalue('id')
-    todo: Todo = db.get_todo(todo_id)
+    todo: Todo = database.get_todo(todo_id)
 
     if todo.is_done < 2:
         todo.is_done += 1
-        db.update_todo(todo)
-
-# das geht erst ab Python 3.10!!!
-#def perform_action(form: cgi.FieldStorage):
-#    action = form.getvalue('action')
-#    match action:
-#        case 'create':
-#            perform_action_create(form)
-#        case 'delete':
-#            perform_action_delete(form)
-#        case 'update':
-#            perform_action_update(form)
-#        case 'shift':
-#            perform_action_shift(form)
+        database.update_todo(todo)
 
 def perform_action(form: cgi.FieldStorage):
+    '''
+    Action from URL validation
+    '''
     action = form.getvalue('action')
+
     if action == 'create':
         perform_action_create(form)
     if action == 'delete':
@@ -90,17 +108,30 @@ def perform_action(form: cgi.FieldStorage):
         perform_action_shift(form)
 
 def draw(form: cgi.FieldStorage):
+    '''
+    Assemble the html document
+    '''
     perform_action(form)
-
+    # For the cgi
     print('Content-Type:text/html\n')
-    print('<!DOCTYPE html>')
-    print('<html lang="de">')
-    print ("<head>")
-    print ("<title>TODO List</title>")
-    print ("</head>")
-    print ("<body>")
+
+    # Begin html output
+    print('''
+    <!DOCTYPE html>
+    <html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="presentation_layer/css/mystyle.css">
+        <title>TODO List</title>
+    </head>
+    
+    <body>
+    ''')
 
     print_todo_list()
 
-    print ("</body>")
-    print ("</html>")
+    print ('''
+    </body>
+    </html>
+    ''')
