c9b27575fa17515a6804ecb22728c30166c1648e
Sebastian Feustel <48125666+53845714nF@users.noreply.github.com>
2022-10-13 12:46:44 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+apt -y update && apt -y upgrade
+apt -y install apache2 apache2-utils python3 && apt-get clean -y
+
+a2enmod cgi
+
+wget https://raw.githubusercontent.com/tnutzmann/OSWC-Semesteraufgabe/main/conf/cgi-apache.conf -O /etc/apache2/conf-available/cgi-enabled.conf
+
+a2enconf cgi-enabled
+
+mkdir -p /var/www/html/cgi && cd /var/www/html/cgi
+
+git clone https://github.com/tnutzmann/OSWC-Semesteraufgabe.git
+mv OSWC-Semesteraufgabe/src/* .
+rm -rf OSWC-Semesteraufgabe/
+
+chmod 707 /var/www/html/cgi
+chmod 705 /var/www/html/cgi/index.cgi
+chmod 666 /var/www/html/cgi/todo_cgi.log
+chmod 666 /var/www/html/cgi/todo.db
+
+systemctl restart apache2
